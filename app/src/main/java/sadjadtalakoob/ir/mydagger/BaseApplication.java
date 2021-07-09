b14332ba720efcc2a755402d1e9a6055d6fe4277
Sadjad <sadjadtalakob74@gmail.com>
2021-07-09 19:47:08 +0400

    First Commit

---
+++ app/src/main/java/sadjadtalakoob/ir/mydagger/BaseApplication.java
@@ -0,0 +1,11 @@
+package sadjadtalakoob.ir.mydagger;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return null;
+    }
+}
